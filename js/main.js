6bb034cd87e1da4d8b81e811954dd50e30f41eec
LetsGoMan <ilikeseik@gmail.com>
2021-12-10 12:39:07 +0300

    finish adaptive

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(function(){
   $('.slider-box').slick({
     prevArrow:  '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
     nextArrow:  '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+    responsive: [
+      {
+      breakpoint: 481,
+      settings: {
+        arrows: false
+      }
+      }
+    ]
   });
   $('.menu-btn').on('click', function() {
     $('.menu__list').toggleClass('active');
